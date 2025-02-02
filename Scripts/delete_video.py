24046f0b19e794b01e793380bce863cef4352b9f
Mohamedjbelii <mohamedjbeli26@gmail.com>
2025-02-02 17:05:10 +0100

    Add trendplus-backend

--- Scripts/delete_video.py
+++ Scripts/delete_video.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python
 
-import httplib2
 import os
 import sys
-from apiclient.discovery import build
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
-from googleapiclient.errors import HttpError
-
-
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2.credentials import Credentials
-TOKEN_FILE = "token_{}.json"
+import argparse
 
+TOKEN_FILE = "token.json"
 CLIENT_SECRETS_FILE = "client_secrets.json"
 YOUTUBE_DELETE_SCOPE = "https://www.googleapis.com/auth/youtube"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-YOUTUBE_API_SCOPE = "https://www.googleapis.com/auth/youtube"
 
 MISSING_CLIENT_SECRETS_MESSAGE = """
 WARNING: Please configure OAuth 2.0
@@ -37,20 +29,23 @@ For more information about the client_secrets.json file format, please visit:
 https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 """ % os.path.abspath(os.path.join(os.path.dirname(__file__), CLIENT_SECRETS_FILE))
 
-def get_authenticated_service(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-    scope=YOUTUBE_DELETE_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+def get_authenticated_service():
+    credentials = None
+    if os.path.exists(TOKEN_FILE):
+        credentials = Credentials.from_authorized_user_file(TOKEN_FILE, [YOUTUBE_DELETE_SCOPE])
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, [YOUTUBE_DELETE_SCOPE])
+            credentials = flow.run_local_server(port=0)
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
+        with open(TOKEN_FILE, 'w') as token:
+            token.write(credentials.to_json())
 
-  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    http=credentials.authorize(httplib2.Http()))
-    
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
+
 def delete_video(youtube, video_id):
     try:
         youtube.videos().delete(id=video_id).execute()
@@ -65,10 +60,11 @@ def delete_video(youtube, video_id):
             print(f"An error occurred: {e.content}")
 
 if __name__ == "__main__":
-    argparser.add_argument("--video-id", required=True, help="The ID of the video to delete")
-    args = argparser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video-id", required=True, help="The ID of the video to delete")
+    args = parser.parse_args()
 
-    youtube = get_authenticated_service(args)
+    youtube = get_authenticated_service()
 
     try:
         delete_video(youtube, args.video_id)
---
+++ Web_App/Backend/trendplus-backend/src/test/java/com/trendplus/trendplus_backend/TrendplusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendplus.trendplus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendplusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
